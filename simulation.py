a3775dc89316e06928285d424df77cf0596b820b
Ivan Smirnov <smirnov.ie@phystech.edu>
2023-03-03 20:57:14 +0300

    final

--- simulation.py
+++ simulation.py
@@ -13,7 +13,9 @@ class Simulation:
         self.initial_stl = Path(Path.cwd(), 'STL')
         self.current_stl = Path(Path.cwd(), 'STL_current')
         shutil.rmtree(self.current_stl, ignore_errors=True)
+        #os.mkdir(str(self.current_stl))
         shutil.copytree(self.initial_stl, self.current_stl)
+        os.system(f"sudo chmod 777 {str(self.current_stl)}")
 
     def tick(self, commands: Commands) -> Sensors:
         self.change_model(commands)
@@ -22,9 +24,15 @@ class Simulation:
         return sensors
 
     def change_model(self, commands: Commands) -> None:
-        r_v = "_".join('0.036011 0 0.0579403'.split())
-        l_v = "_".join('-0.000188 0.0540462 -0.0029555'.split())
-        os.system(f"./transform {str(Path(self.initial_stl, 'right.stl'))} {str(Path(self.current_stl, 'right.stl'))} {r_v} {commands.first_motor} {str(Path(self.initial_stl, 'left.stl'))} {str(Path(self.current_stl, 'left.stl'))} {l_v} {commands.second_motor}")
+        r_c = "_".join('0.119277 0.0585911 -0.012426'.split())
+        l_c = "_".join('0.119277 -0.0585911 -0.012426'.split())
+        r_v = "_".join('0.000188 0.0540462 0.0029555'.split())
+        l_v = "_".join('0.000188 0.0540462 -0.0029555'.split())
+        right_cur = str(Path(self.current_stl, 'right.stl'))
+        left_cur = str(Path(self.current_stl, 'left.stl'))
+        right_init = str(Path(self.initial_stl, 'right.stl'))
+        left_init = str(Path(self.initial_stl, 'left.stl'))
+        os.system(f"./transform {right_init} {right_cur} {r_c} {r_v} {commands.second_motor} {left_init} {left_cur} {l_c} {l_v} {commands.first_motor}")
     def compute_model(self) -> None:
         os.system(f'pvpython render2.py -p {self.current_stl}')
 
--- transform.py
+++ transform.py
@@ -5,14 +5,19 @@ import sys
 
 right_init = sys.argv[1]
 right_cur = sys.argv[2]
-right_vector = " ".join(sys.argv[3].split('_'))
-right_angle = sys.argv[4]
+right_center = " ".join(sys.argv[3].split('_'))
+right_vector = " ".join(sys.argv[4].split('_'))
+right_angle = sys.argv[5]
 
-left_init = sys.argv[5]
-left_cur = sys.argv[6]
-left_vector = " ".join(sys.argv[7].split('_'))
-left_angle = sys.argv[8]
+left_init = sys.argv[6]
+left_cur = sys.argv[7]
+left_center = " ".join(sys.argv[8].split('_'))
+left_vector = " ".join(sys.argv[9].split('_'))
+left_angle = sys.argv[10]
+
+os.system(f"surfaceTransformPoints  -rotate-angle '(({right_vector}) {right_angle})' {right_init} {right_cur}")
+os.system(f"surfaceTransformPoints  -rotate-angle '(({left_vector}) {left_angle})' {left_init} {left_cur}")
 
 
-os.system("surfaceTransformPoints -rotate-angle '(("+ str(right_vector) +")" + right_angle + ")' " + right_init + " " + right_cur)
-os.system("surfaceTransformPoints -rotate-angle '(("+ str(left_vector) +")" + left_angle + ")' " + left_init + " " + left_cur)
+#os.system(f"surfaceTransformPoints -centre '({right_center})' -rotate-angle '(({right_vector}) {right_angle})' {right_init} {right_cur}")
+#os.system(f"surfaceTransformPoints -centre '({left_center})' -rotate-angle '(({left_vector}) {left_angle})' {left_init} {left_cur}")
